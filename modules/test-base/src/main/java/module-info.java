9f7872e3247b3884514abe02815e243d215bec50
Marcel Witte <marcel@aliquis.de>
2022-08-09 23:27:20 +0200

    build and deploy apidocs

---
+++ modules/test-base/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cloud.quinimbus.persistence.testbase {
+    
+    requires cloud.quinimbus.persistence.api;
+    requires cloud.quinimbus.persistence.common;
+    requires org.junit.jupiter.api;
+    requires throwing.interfaces;
+    requires throwing.streams;
+}
