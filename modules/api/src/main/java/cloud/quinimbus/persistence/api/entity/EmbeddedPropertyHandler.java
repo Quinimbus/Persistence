2efe8f47d0554a1ff4810139f1c7179cdc546e1f
Marcel Witte <marcel@aliquis.de>
2024-07-08 03:48:31 +0200

    added utility method to create a new embedded object in EmbeddedPropertyHandler

--- modules/api/src/main/java/cloud/quinimbus/persistence/api/entity/EmbeddedPropertyHandler.java
+++ modules/api/src/main/java/cloud/quinimbus/persistence/api/entity/EmbeddedPropertyHandler.java
@@ -2,6 +2,8 @@ package cloud.quinimbus.persistence.api.entity;
 
 import cloud.quinimbus.persistence.api.PersistenceContext;
 import cloud.quinimbus.persistence.api.lifecycle.LifecycleEvent;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import lombok.Getter;
 
@@ -28,6 +30,19 @@ public abstract class EmbeddedPropertyHandler {
 
     public <T extends LifecycleEvent> void onLifecycleEvent(Class<T> eventType, Consumer<T> consumer) {
         this.context.onLifecycleEvent(schemaId, eventType, typeId, consumer);
+    }
+
+    public EmbeddedObject newEmbedded(Map<String, Object> properties, Map<String, Object> transientFields)
+            throws UnparseableValueException {
+        var schema = context.getSchema(this.schemaId)
+                .orElseThrow(() -> new IllegalStateException("Schema % not found.".formatted(this.schemaId)));
+        var parentType = schema.entityTypes().get(this.typeId);
+        var type = parentType
+                .embeddedPropertyType(this.property)
+                .orElseThrow(() ->
+                        new IllegalStateException("Embedded type for property %s not found on type %s in schema %s."
+                                .formatted(this.property, this.typeId, this.schemaId)));
+        return this.context.newEmbedded(type, parentType, List.of(this.property), properties, transientFields);
     }
 
     public abstract void init();
